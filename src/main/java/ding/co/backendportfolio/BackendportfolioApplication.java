7c014aa3aedaa6af96190876235612d5daa44b59
yoon <stay_on_track@naver.com>
2026-04-02 09:32:11 +0900

    Git Action Workflow Test

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -1,5 +1,6 @@
 package ding.co.backendportfolio;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,8 +9,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
+@Log4j2
 public class BackendportfolioApplication {
     public static void main(String[] args) {
+        log.info("Main Branch PR -> Git Action Workflow Test!!");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
