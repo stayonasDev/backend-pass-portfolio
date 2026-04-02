9a09eaaac3274bcacc3ec80788fe19f1117f3c23
yoon <stay_on_track@naver.com>
2026-04-02 10:46:18 +0900

    AWS SSM 접속 시 Docker 미 설치 및 컨테이너 없음 확인 CI/CD 재시도

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -1,6 +1,5 @@
 package ding.co.backendportfolio;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,10 +8,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
-@Log4j2
 public class BackendportfolioApplication {
     public static void main(String[] args) {
-        log.info("Main Branch PR -> Git Action Workflow Test!!");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
